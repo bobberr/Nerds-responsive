b69b74cdde09248bfe7a653a71e9b7a15d373259
bobberr <izotopenko@yandex.ru>
2016-09-11 22:55:21 +0300

    First commit

---
+++ js/js.js
@@ -0,0 +1,9 @@
+$('.slider_block').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  arrows: false
+});
---
+++ js/menu.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#burger_nav").on("click", function(){
+       $(".main_header ul").toggleClass("open");
+    });
+});
